7c7967cd244ef2b2f211837789bf4258ad61cf73
Branco <fausto.branco@kuehne-nagel.com>
2026-02-06 14:12:48 +0000

    [FEAT] - 2026-02-06 14:12:48

---
+++ Formula/pssql.rb
@@ -0,0 +1,20 @@
+class Pssql < Formula
+  desc "PostgreSQL Connection Manager"
+  homepage "https://github.com/faustobranco/pssql"
+  url "https://github.com/faustobranco/pssql/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "ea6648eb363bd72b7d4c4cf07c850e70751037bd5787e47d2b20bb7c24b1825f"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", bin/"pssql", "."
+    
+    zsh_completion.install "completion/_pssql.zsh" => "_pssql"
+    bash_completion.install "completion/_pssql.bash" => "pssql"
+  end
+
+  test do
+    system "#{bin}/pssql", "--version"
+  end
+end
