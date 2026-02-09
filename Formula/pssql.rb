d49e100ace0f8ae261aadd12717adef683f8204e
Branco <fausto.branco@kuehne-nagel.com>
2026-02-09 14:44:39 +0000

    [FEAT] - 2026-02-09 14:44:39 - Added AWS/RDS IAM login

--- Formula/pssql.rb
+++ Formula/pssql.rb
@@ -1,8 +1,8 @@
 class Pssql < Formula
   desc "PostgreSQL Connection Manager"
   homepage "https://github.com/faustobranco/pssql"
-  url "https://github.com/faustobranco/pssql/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "268014168abf272766bc7e3ae112430c928ba4b700b187db3c1a2828ec45c64c"
+  url "https://github.com/faustobranco/pssql/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "31d2e78556d28fcd51169f294add421066a254cdaa71fe61e96ca498b01d6d0e"
   license "MIT"
 
   depends_on "go" => :build
