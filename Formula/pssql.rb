6ba5578df0526fc22a620a8f33592e1cf442909a
Branco <fausto.branco@kuehne-nagel.com>
2026-02-09 15:12:52 +0000

    [FEAT] - 2026-02-09 15:12:52 - Fix dependencies

--- Formula/pssql.rb
+++ Formula/pssql.rb
@@ -1,8 +1,8 @@
 class Pssql < Formula
   desc "PostgreSQL Connection Manager"
   homepage "https://github.com/faustobranco/pssql"
-  url "https://github.com/faustobranco/pssql/archive/refs/tags/v1.1.2.tar.gz"
-  sha256 "c2e5169a6aeee34e46f76264af5e393dcc586b79071ea3b076e25c76c3aa281c"
+  url "https://github.com/faustobranco/pssql/archive/refs/tags/v1.1.3.tar.gz"
+  sha256 "e39349b3c8cece65ccdbde77eed79f77f3c37cb2bb6d3193ab4a07c1b8213246"
   license "MIT"
 
   depends_on "go" => :build
